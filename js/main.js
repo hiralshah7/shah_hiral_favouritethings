229aea2a7f92443f708075b6f945dd7e17648a89
hiralshah7 <90278193+hiralshah7@users.noreply.github.com>
2022-10-16 01:57:20 -0400

    updating_files

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 //this is called an IIFE (Inmediatly Invoked Function Expression)
 import { getData } from "./modules/dataMiner.js";
 
+
 // adding the script below : 
 (() => {
     console.log('fired!');
@@ -9,40 +10,52 @@ import { getData } from "./modules/dataMiner.js";
         theTemplate = document.querySelector('#fav-template').content;
     const thebtn = document.querySelectorAll(".btn");
 
+
+
     function buildTeam(data) {
         //get all the keys (names) from the data object and use that to iterate through the data
         // debugger;
         const people = Object.keys(data); // Object.keys creates an array
 
         people.forEach(prof => {
-            // copy the template's contents
-            let panel = theTemplate.cloneNode(true);
 
-            // get a reference to the template's elements
-            let containers = panel.firstElementChild.children;
+                // copy the template's contents
+                let panel = theTemplate.cloneNode(true);
 
-            // grab the image from the object and set it as the source
-            containers[0].querySelector('img').src = `images/${data[prof].fav_images}`;
+                // get a reference to the template's elements
+                let containers = panel.firstElementChild.children;
 
-            containers[1].textContent = data[prof].favourite;
-            containers[2].textContent = data[prof].info;
-            containers[3].textContent = data[prof].nickname;
+                // grab the image from the object and set it as the source
+                containers[0].querySelector('img').src = `images/${data[prof].fav_images}`;
 
-            theTeam.appendChild(panel);
+                containers[1].textContent = data[prof].favourite;
+                containers[2].textContent = data[prof].info;
+                containers[3].textContent = data[prof].nickname;
 
-            // adding the button function 
-            // debugger;
+                theTeam.appendChild(panel);
 
-            function showData() {
-                containers[0].querySelector('img').src = `images/${data[prof].picture}`;
-            }
+                // adding the button function 
+                // debugger;
 
-            thebtn.forEach(button => button.addEventListener("click", showData));
+                function showData() {
 
-        })
+                    containers[0].querySelector('img').src = `images/${data[prof].picture}`;
+                }
+
+
+
+                thebtn.forEach(button => button.addEventListener("click", showData));
+
+            })
+            // let Draggable = document.querySelectorAll('#fav_info');
+
+
     }
 
 
 
     getData(buildTeam);
+
+
+
 })();
